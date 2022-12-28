0d5ab1a5260fe3a7f1c547653ba0bf45c0f8cfc3
nizwant <mateusznizwantowski@gmail.com>
2022-12-28 22:01:19 +0100

    test file added

---
+++ src/Test.java
@@ -0,0 +1,7 @@
+
+public class Test {
+    public static void main(String[] args) {
+        String username = API_credentials.username;
+        String password = API_credentials.password;
+    }
+}
