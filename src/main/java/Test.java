f2f4e4d001010cfe30ddad96d9160fd3c49cea1a
nizwant <mateusznizwantowski@gmail.com>
2023-01-01 17:41:46 +0100

    bbox added

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,16 +1,18 @@
-import org.opensky.api.OpenSkyApi;
+import opensky.api.OpenSkyApi;
 import org.opensky.model.OpenSkyStates;
+import org.opensky.model.StateVector;
 
 import java.io.IOException;
+import java.util.Collection;
 
 public class Test {
     public static void main(String[] args) throws IOException {
         String username = API_credentials.username;
         String password = API_credentials.password;
-        System.out.println(username);
-        System.out.println(password);
         OpenSkyApi api = new OpenSkyApi(username, password);
-        OpenSkyStates os = api.getStates(0, null, null);
-        System.out.println(os.getStates());
+        OpenSkyApi.BoundingBox box = new OpenSkyApi.BoundingBox(49.0273953314, 54.8515359564, 14.0745211117,24.0299857927);
+        OpenSkyStates os = api.getStates(0, null, box);
+        Collection<StateVector> planes_poland = os.getStates();
+        System.out.println(planes_poland);
     }
 }
