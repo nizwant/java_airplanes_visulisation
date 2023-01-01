f11d0941c1794668d9662a5c11bde6c08317daa5
nizwant <mateusznizwantowski@gmail.com>
2023-01-01 15:29:12 +0100

    restructured project to maven

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,5 +3,7 @@ public class Test {
     public static void main(String[] args) {
         String username = API_credentials.username;
         String password = API_credentials.password;
+        System.out.println(username);
+        System.out.println(password);
     }
 }
