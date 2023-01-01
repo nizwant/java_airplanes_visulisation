af14ba616e879b387d4bfc4f42907f9986c9506a
nizwant <mateusznizwantowski@gmail.com>
2023-01-01 16:49:29 +0100

    getting data from api

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,9 +1,16 @@
+import org.opensky.api.OpenSkyApi;
+import org.opensky.model.OpenSkyStates;
+
+import java.io.IOException;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String username = API_credentials.username;
         String password = API_credentials.password;
         System.out.println(username);
         System.out.println(password);
+        OpenSkyApi api = new OpenSkyApi(username, password);
+        OpenSkyStates os = api.getStates(0, null, null);
+        System.out.println(os.getStates());
     }
 }
