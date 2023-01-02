6d01378a650f0c9a37044350711404c88a13f779
nizwant <mateusznizwantowski@gmail.com>
2023-01-02 22:20:18 +0100

    now it should be working kuba rymarski

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,9 @@
-import org.opensky.model.StateVector;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Arrays;
-import java.util.Collection;
 
 
 public class Test {
