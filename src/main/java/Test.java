5a40943e452b0a53d9b85b9af8da8f3e0d475490
nizwant <mateusznizwantowski@gmail.com>
2023-01-02 22:00:01 +0100

    new way of making http requests-no need for maven

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,14 +1,38 @@
 import org.opensky.model.StateVector;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Arrays;
 import java.util.Collection;
+
 
 public class Test {
     public static void main(String[] args) throws IOException {
 
-        Collection<StateVector> planes_poland = Get_api_data.refreh();
-        for (StateVector s:planes_poland) {
-            System.out.println(s);
+        // http request
+        URL url = new URL("https://opensky-network.org/api/states/all?lamin=49.0273953314&lomin=14.0745211117&lamax=54.8515359564&lomax=24.0299857927");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder sb = new StringBuilder();
+        while ((inputLine = br.readLine()) != null) {
+            sb.append(inputLine);
+        }
+        br.close();
+        con.disconnect();
+
+        // string operation
+        String data_in_string = sb.toString();
+        String sub_data = data_in_string.substring(29, data_in_string.length() - 3);
+        String[] array_of_strings_with_planes = sub_data.split("],");
+        for (String plane_str: array_of_strings_with_planes) {
+            plane_str = plane_str.substring(1);
+            String[] ss = plane_str.split(",");
+            System.out.println(Arrays.toString(ss));
         }
     }
 }
