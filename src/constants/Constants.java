2a2d728cf575ce47ac447ac47cebc611c5e5ec75
nizwant <mateusznizwantowski@gmail.com>
2023-01-22 23:11:23 +0100

    performence improvment
    lower resolution map + fast scaling

--- src/constants/Constants.java
+++ src/constants/Constants.java
@@ -11,18 +11,18 @@ public class Constants {
 
     public static double SCALE_BOOST = 50; // %
     public static int MAP_MOVE_STEP = 100; // flat
-    public static int MAP_SCALING_ALGORITHM = Image.SCALE_DEFAULT;
-    public static double INIT_SCALE = 0.1955;
+    public static int MAP_SCALING_ALGORITHM = Image.SCALE_FAST;
+    public static double INIT_SCALE = 0.3534;
     public static int INIT_MAP_POS_X = 0;
     public static int INIT_MAP_POS_Y = 0;
     public static int ZOOM_IN_COUNT_LIMIT = 5;
 
     public static String RESOURCES_PATH = "./resources";
-    public static String MAP_NAME = "satellite.png";
+    public static String MAP_NAME = "satellite3.png";
 
-    public static int REFRESH_DELAY_IN_SECONDS = 5;
+    public static int REFRESH_DELAY_IN_SECONDS = 10;
 
-    public static boolean USE_TEST_DATA = true;
+    public static boolean USE_TEST_DATA = false;
 
     public static int INTERACTABLE_OBJECT_BUTTON_WIDTH = 50;
     public static int INTERACTABLE_OBJECT_BUTTON_HEIGHT = 50;
@@ -33,10 +33,11 @@ public class Constants {
     public static double POLAND_THE_NORTHERNMOST_POINT_LATITUDE = 60;
     public static double POLAND_THE_SOUTHERNMOST_POINT_LATITUDE = 40;
 
-    public static double LEFT_BORDER_LONGITUDE = 12.688770;
-    public static double RIGHT_BORDER_LONGITUDE = 25.407964;
-    public static double TOP_BORDER_LATITUDE = 55.757437;
-    public static double BOTTOM_BORDER_LATITUDE = 48.310404;
+    public static double LEFT_BORDER_LONGITUDE = 13.632292;
+    public static double RIGHT_BORDER_LONGITUDE = 24.520718;
+    public static double TOP_BORDER_LATITUDE = 55.249618;
+    public static double BOTTOM_BORDER_LATITUDE = 48.760368;
+
 
 
     public static boolean USER_POLAND_S_MAP_BOUNDS = false;
